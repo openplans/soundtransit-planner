4bdf56d15ecd836122e5a135e33e4caa7f5ce9ec
jmaki <jmaki@jmakis-MacBook-Pro.local>
2010-12-14 11:58:12 -0500

    Map fix for IE

--- comps2/js/map.js
+++ comps2/js/map.js
@@ -4,7 +4,8 @@ OTP.Map = function(_root) {
     if(typeof _root === 'undefined' || _root === null) {
         return null;
     }
-    
+return null;
+
     var root = jQuery(_root);
     var map = null;
 
@@ -51,13 +52,14 @@ OTP.Map = function(_root) {
         controls: [
             new OpenLayers.Control.Navigation(),
             new OpenLayers.Control.KeyboardDefaults(),
-            new OpenLayers.Control.PanZoomBar({zoomWorldIcon:false}),
+            new OpenLayers.Control.PanZoomBar({zoomWorldIcon:false})
         ]
     });
 
     // this points OL to our custom pan/zoom icon set
     OpenLayers.ImgPath = "js/openlayers/img/";
-    
+
+
     // add bing baselayers
     var shaded = new OpenLayers.Layer.VirtualEarth("Shaded", {
         type: VEMapStyle.Shaded,
