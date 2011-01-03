10d5d44387d17f2c75da5a6786923c7b9dc45691
jmaki <jmaki@jmakis-MacBook-Pro.local>
2011-01-03 18:47:05 -0500

    Page index

---
+++ frontend/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Homepage</title>
+
+    <!--INCLUDE ALL OF THESE FILES IN A PRODUCTION DEPLOYMENT, IN THIS ORDER-->
+	<link type="text/css" href="css/screen-reset.css" rel="stylesheet" media="screen, projection" />
+	<link type="text/css" href="css/print-reset.css" rel="stylesheet" media="print" />
+	<link type="text/css" href="css/jquery/ui-custom/jquery-ui-1.8.6.custom.css" rel="stylesheet" media="screen, projection" />
+    <link type="text/css" href="css/jquery/ui.combobox.css" rel="stylesheet" media="screen, projection" />
+	<link type="text/css" href="css/jquery/ui.spinner.css" rel="stylesheet" media="screen, projection" />
+	<link type="text/css" href="css/jquery/ui.combobox.css" rel="stylesheet" media="screen, projection" />
+	<link type="text/css" href="css/otp.css" rel="stylesheet" media="screen, projection" />
+    <!--[if lt IE 8]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection"><![endif]-->  
+
+    <!--DO NOT INCLUDE THE FILE BELOW IN A PRODUCTION DEPLOYMENT - FOR DEMO ONLY-->
+	<link type="text/css" href="css/demo.css" rel="stylesheet" media="screen, projection" />
+</head>
+
+<body class="home">
+    <div id="plannerform">
+      <h2>Page Index</h2>
+
+      <p>
+      <a href="home.php">Homepage</a>
+      </p>
+
+      <p>
+      <a href="map.php">System Map</a>
+      </p>
+
+      <p>
+      <a href="tripplanner.php">Trip Planner</a>
+      </p>
+    </div>    
+</body>
+</html>
