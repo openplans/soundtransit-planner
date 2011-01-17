e958528a24663b0fe7fe5aff90fb498a35485adc
jmaki <jmaki@jmakis-MacBook-Pro.local>
2011-01-17 14:30:06 -0500

    Cleanup of old files no longer needed; added config.js to contain deployment-specific config params.

---
+++ frontend/routemaps.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=8" />
+    <title>Route Maps</title>
+
+    <!--INCLUDE ALL OF THESE FILES IN A PRODUCTION DEPLOYMENT, IN THIS ORDER-->
+    <link type="text/css" href="css/screen-reset.css" rel="stylesheet" media="screen, projection" />
+    <link type="text/css" href="css/print-reset.css" rel="stylesheet" media="print" />
+    <link type="text/css" href="css/jquery/ui-custom/jquery-ui-1.8.6.custom.css" rel="stylesheet" media="screen, projection" />
+    <link type="text/css" href="css/jquery/ui.selectmenu.css" rel="stylesheet" media="screen, projection" />
+    <link type="text/css" href="css/jquery/ui.spinner.css" rel="stylesheet" media="screen, projection" />
+    <link type="text/css" href="css/otp.css" rel="stylesheet" media="screen, projection" />
+    <!--[if lt IE 8]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection"><![endif]-->
+
+    <!--DO NOT INCLUDE THE FILE BELOW IN A PRODUCTION DEPLOYMENT - FOR DEMO ONLY-->
+    <link type="text/css" href="css/demo.css" rel="stylesheet" media="screen, projection" />
+
+    <!--INCLUDE ALL OF THESE FILES IN A PRODUCTION DEPLOYMENT, IN THIS ORDER-->
+    <script type="text/javascript" src="js/jquery/jquery-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/jquery/jquery-jsonp.min.js"></script>
+    <script type="text/javascript" src="js/jquery/jquery-ui-1.8.6.custom.min.js"></script>
+    <script type="text/javascript" src="js/jquery/jquery-ui-selectmenu.min.js"></script>
+    <script type="text/javascript" src="js/jquery/jquery-ui-spinner.min.js"></script>
+    <script type="text/javascript" src="js/openlayers/OpenLayers.js"></script>
+    <script type="text/javascript" src="js/config.js"></script>
+    <script type="text/javascript" src="js/narrativeForm.js"></script>
+    <script type="text/javascript" src="js/narrative.js"></script>
+    <script type="text/javascript" src="js/map.js"></script>
+</head>
+
+<body class="home">
+
+    <div>
+      <h2>Direct links for Route Map pages</h2>
+      <ul>
+        <li><a href="./map.php?SOUNDERRoute=MSOUNDER&amp;SOUNDERStops=7">Sounder: Tacoma/Seattle route</a></li>
+        <li><a href="./map.php?SOUNDERRoute=MSOUNDER&amp;SOUNDERStops=4">Sounder: Everett/Seattle route</a></li>
+        <li><a href="./map.php?LINKRoute=M599">Central Link Light Rail</a></li>
+        <li><a href="./map.php?LINKRoute=PTLDTC">Everett/Seattle route</a></li>
+        <li><a href="./map.php?BUSOperator=ST&amp;BUSRoute=510">Bus Route 510</a></li>
+        <li><a href="./map.php?BUSOperator=ST&amp;BUSRoute=P590">Bus Route 590</a></li>
+      </ul>
+    </div>
+    
+</body>
+</html>
